35b84f294e2a2ac1cee7d8e7d9d5f357af85f4b4
Lauren Templeton <etothel0514@gmail.com>
2016-10-09 14:31:45 -0400

    Added images for states and fixes to trivia section
    Added images for each state and fixes to trivia buttons section in the
    askQuestion function

--- Code/script.js
+++ Code/script.js
@@ -1,16 +1,16 @@
 //might remove alt
 var destinationsData = [
-    { name: 'New York', X: 1175, Y: 260, triviaQuestion: 'There are over 840 miles of subway track in New York City.', correctAnswer: 'True' },
-    { name: 'Pennsylvania', X: 1060, Y: 300, triviaQuestion: 'Hershey, PA is nicknamed "The Sweetest Place On Earth"', correctAnswer: 'True' },
-    { name: 'Ohio', X: 1000, Y: 330, triviaQuestion: 'Cleveland was the first city in the world to have a public school system.', correctAnswer: 'False', alt: 'Become lighted electrically' },
-    { name: 'Indiana', X: 925, Y: 330, triviaQuestion: 'Santa Claus,IN receives over a half million letters and requests at Christmastime', correctAnswer: 'True' },
-    { name: 'Illinois', X: 860, Y: 330, triviaQuestion: '"Twerking" is the official state dance of Illinois.', correctAnswer: 'False', alt: 'Square dance' },
-    { name: 'Iowa', X: 755, Y: 300, triviaQuestion: 'The "Honeycrisp" variety of apple originated in Peru, Iowa', correctAnswer: 'False', alt: 'Red Delicious apple' },
-    { name: 'Nebraska', X: 600, Y: 325, triviaQuestion: 'The Nebraska National Forest is the largest hand-planted forest in America.', correctAnswer: 'True'},
-    { name: 'Wyoming', X: 450, Y: 300, triviaQuestion: 'The majority of The Grand Canyon is in Wyoming', correctAnswer: 'False', alt: 'Yellowstone National Park'},
-    { name: 'Utah', X: 320, Y: 330, triviaQuestion: 'The inventor of the television was born in Beaver, UT', correctAnswer: 'True'},
-    { name: 'Nevada', X: 230, Y: 330, triviaQuestion: 'Las Vegas has over 300,000 hotel rooms', correctAnswer: 'False', alt: '150,000 - still more than any city on earth'},
-    { name: 'California', X: 150, Y: 430, triviaQuestion: 'California is home to the highest and lowest points in the continental U.S.', correctAnswer: 'True'}
+    { name: 'New York', X: 1175, Y: 260, triviaQuestion: 'There are over 840 miles of subway track in New York City.', correctAnswer: 'True', imageName: 'nyc.jpg'},
+    { name: 'Pennsylvania', X: 1060, Y: 300, triviaQuestion: 'Hershey, PA is nicknamed "The Sweetest Place On Earth"', correctAnswer: 'True', imageName: 'hersheypa.jpg' },
+    { name: 'Ohio', X: 1000, Y: 330, triviaQuestion: 'Cleveland was the first city in the world to have a public school system.', correctAnswer: 'False', imageName: 'clevelandoh.jpg' },
+    // { name: 'Indiana', X: 925, Y: 330, triviaQuestion: 'Santa Claus,IN receives over a half million letters and requests at Christmastime', correctAnswer: 'True', imageName: 'indiana.jpg' },
+    // { name: 'Illinois', X: 860, Y: 330, triviaQuestion: '"Twerking" is the official state dance of Illinois.', correctAnswer: 'False', imageName: 'illinois.jpg' },
+    // { name: 'Iowa', X: 755, Y: 300, triviaQuestion: 'The "Honeycrisp" variety of apple originated in Peru, Iowa', correctAnswer: 'False', imageName: 'iowa.jpg' },
+    // { name: 'Nebraska', X: 600, Y: 325, triviaQuestion: 'The Nebraska National Forest is the largest hand-planted forest in America.', correctAnswer: 'True', imageName: 'nebraska.jpg'},
+    // { name: 'Wyoming', X: 450, Y: 300, triviaQuestion: 'The majority of The Grand Canyon is in Wyoming', correctAnswer: 'False', imageName: 'wyoming.jpg' },
+    // { name: 'Utah', X: 320, Y: 330, triviaQuestion: 'The inventor of the television was born in Beaver, UT', correctAnswer: 'True', imageName: 'utah.jpg'},
+    // { name: 'Nevada', X: 230, Y: 330, triviaQuestion: 'Las Vegas has over 300,000 hotel rooms', correctAnswer: 'False', imageName: 'lasvegas.jpg' },
+    // { name: 'California', X: 150, Y: 430, triviaQuestion: 'California is home to the highest and lowest points in the continental U.S.', correctAnswer: 'True', imageName: 'california.jpeg'}
 ];
 
 //make a copy of destinationsData to work with the data called destinations
@@ -40,12 +40,18 @@ function go() {
                 checkAnswer('timer');
             }, timeToAnswer + moveSpeed);
         }
+        //if player reaches the end
+        else {
+            displayGameState('#congrats');
+        }
     }
 }
 
 function askQuestion(destination) {
     displayGameState('#trivia');
     $('.trivia').text(destination.triviaQuestion);
+    $('#trivia img').attr({src: '../Images/Stops/' + destination.imageName, alt: destination.name});
+    $('#trivia input[name="answer"]').prop('checked', false);
 
     //start + display timer
     initializeTimer('#timer', timeToAnswer);
@@ -65,7 +71,7 @@ function displayGameState(selector){
 function checkAnswer(source) {
     var answer = $('input:radio[name=answer]:checked').val();
 
-    if (answer === currentStop.correctAnswer) {
+    if (currentStop && answer === currentStop.correctAnswer) {
         // move to next
         go();
     }
