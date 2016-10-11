265de223bba93942d28f3cf57089ea88099c9553
Lauren Templeton <etothel0514@gmail.com>
2016-10-10 20:20:37 -0400

    finalized everything for submission
    formatted code for submission

--- Code/script.js
+++ Code/script.js
@@ -1,16 +1,16 @@
 
 var destinationsData = [
-    { name: 'New York', X: 1029, Y: 210, triviaQuestion: 'There are over 840 miles of subway track in New York City.', correctAnswer: 'True', imageName: 'nyc.jpg'},
+    { name: 'New York', X: 1029, Y: 210, triviaQuestion: 'There are over 840 miles of subway track in New York City.', correctAnswer: 'True', imageName: 'nyc.jpg' },
     { name: 'Pennsylvania', X: 940, Y: 243, triviaQuestion: 'Hershey, PA is nicknamed "The Sweetest Place On Earth"', correctAnswer: 'True', imageName: 'hersheypa.jpg' },
     { name: 'Ohio', X: 887, Y: 268, triviaQuestion: 'Cleveland was the first city in the world to have a public school system.', correctAnswer: 'False', imageName: 'clevelandoh.jpg' },
-    { name: 'Indiana', X:815, Y: 271, triviaQuestion: 'Santa Claus,IN receives over a half million letters and requests at Christmastime', correctAnswer: 'True', imageName: 'indiana.jpg' },
+    { name: 'Indiana', X: 815, Y: 271, triviaQuestion: 'Santa Claus,IN receives over a half million letters and requests at Christmastime', correctAnswer: 'True', imageName: 'indiana.jpg' },
     { name: 'Illinois', X: 755, Y: 268, triviaQuestion: '"Twerking" is the official state dance of Illinois.', correctAnswer: 'False', imageName: 'illinois.jpg' },
     { name: 'Iowa', X: 674, Y: 250, triviaQuestion: 'The "Honeycrisp" variety of apple originated in Peru, Iowa', correctAnswer: 'False', imageName: 'iowa.jpg' },
-    { name: 'Nebraska', X: 551, Y: 264, triviaQuestion: 'The Nebraska National Forest is the largest hand-planted forest in America.', correctAnswer: 'True', imageName: 'nebraska.jpg'},
+    { name: 'Nebraska', X: 551, Y: 264, triviaQuestion: 'The Nebraska National Forest is the largest hand-planted forest in America.', correctAnswer: 'True', imageName: 'nebraska.jpg' },
     { name: 'Wyoming', X: 392, Y: 239, triviaQuestion: 'The majority of The Grand Canyon is in Wyoming', correctAnswer: 'False', imageName: 'wyoming.jpg' },
-    { name: 'Utah', X: 296, Y: 279, triviaQuestion: 'The inventor of the television was born in Beaver, UT', correctAnswer: 'True', imageName: 'utah.jpg'},
+    { name: 'Utah', X: 296, Y: 279, triviaQuestion: 'The inventor of the television was born in Beaver, UT', correctAnswer: 'True', imageName: 'utah.jpg' },
     { name: 'Nevada', X: 200, Y: 284, triviaQuestion: 'Las Vegas has over 300,000 hotel rooms', correctAnswer: 'False', imageName: 'lasvegas.jpg' },
-    { name: 'California', X: 138, Y: 368, triviaQuestion: 'California is home to the highest and lowest points in the continental U.S.', correctAnswer: 'True', imageName: 'california.jpeg'}
+    { name: 'California', X: 138, Y: 368, triviaQuestion: 'California is home to the highest and lowest points in the continental U.S.', correctAnswer: 'True', imageName: 'california.jpeg' }
 ];
 
 //make a copy of destinationsData to work with the data called destinations
@@ -23,7 +23,7 @@ var answerTimer;
 var carOffsetX = -30;
 var carOffsetY = -95;
 var carInitialX = 1180;
-var carInitialY = 111; 
+var carInitialY = 111;
 
 function go() {
     if (destinations) {
@@ -58,7 +58,7 @@ function askQuestion(destination) {
     showModal();
     displayGameState('#trivia');
     $('.trivia').text(destination.triviaQuestion);
-    $('#trivia img').attr({src: '../Images/Stops/' + destination.imageName, alt: destination.name});
+    $('#trivia img').attr({ src: '../Images/Stops/' + destination.imageName, alt: destination.name });
     $('#trivia input[name="answer"]').prop('checked', false);
 
     //start + display timer
@@ -66,7 +66,7 @@ function askQuestion(destination) {
 }
 
 //displays game state to player based on different states of game 
-function displayGameState(selector){
+function displayGameState(selector) {
     //hide all 
     $('.gameState').hide();
     //display selector
@@ -82,11 +82,11 @@ function checkAnswer(source) {
         go();
     }
     else {
-        if (source === 'timer'){
-         displayGameState('#timesUp');
+        if (source === 'timer') {
+            displayGameState('#timesUp');
         }
         else {
-         displayGameState('#wrongAnswer');
+            displayGameState('#wrongAnswer');
         }
     }
 }
@@ -115,46 +115,46 @@ function initializeTimer(id, duration) {
 }
 
 //open and close modal
-function showModal(){
+function showModal() {
     $('#modal').dialog("open");
 
 }
 
-function closeModal(){
+function closeModal() {
     $('#modal').dialog('close');
 }
 
 //for getting form data from url
 function getQueryStringObject() {
-  var queryString = window.location.search.substr(1).split('&'); 
-  var data = {};
+    var queryString = window.location.search.substr(1).split('&');
+    var data = {};
 
-  for (var i = 0; i < queryString.length; i++){
-    // k=v
-    var parts = queryString[i].split('=');
-    if (parts.length === 2){
-      var key = parts[0];
-      var value = parts[1];
-      data[key] = value;
+    for (var i = 0; i < queryString.length; i++) {
+        // k=v
+        var parts = queryString[i].split('=');
+        if (parts.length === 2) {
+            var key = parts[0];
+            var value = parts[1];
+            data[key] = value;
+        }
     }
-  }
 
-  return data;
+    return data;
 }
 
 //resets destinations, moves car back to starting position and closes modal...brings player back to initial game state
 function restart() {
     destinations = destinationsData.slice(0);
-     $('.car').animate({ left: carInitialX + 'px', top: carInitialY + 'px' }, moveSpeed);
-     closeModal();
+    $('.car').animate({ left: carInitialX + 'px', top: carInitialY + 'px' }, moveSpeed);
+    closeModal();
 }
 
 $(function () {
     //initialize modal
-    $('#modal').dialog({ autoOpen: false, modal: true, width: 600});
+    $('#modal').dialog({ autoOpen: false, modal: true, width: 600 });
 
     //begin to run 'go' function when ready button clicked
-    $('#startGameButton').on('click', function(){
+    $('#startGameButton').on('click', function () {
         go();
     });
 
@@ -170,7 +170,7 @@ $(function () {
 
     //event listener for play again button
     $('button.restart').on('click', restart);
-    
+
 
 });
 
